e5c3268bcbb08ca823af54b408eeb08c848da11c
facutopa <topafacundo@gmail.com>
2022-11-21 15:51:39 -0300

    Initial commit

---
+++ app/src/main/java/com/progavanz2/tpfinal_rompecabezas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.progavanz2.tpfinal_rompecabezas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
