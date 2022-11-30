fb62dd308170953f3d984e4bdd0e4e97b37389df
facutopa <topafacundo@gmail.com>
2022-11-30 00:13:14 -0300

    10ª Se agregaron comentarios

--- app/src/main/java/com/progavanz2/tpfinal_rompecabezas/Juego.java
+++ app/src/main/java/com/progavanz2/tpfinal_rompecabezas/Juego.java
@@ -44,16 +44,53 @@ public class Juego extends AppCompatActivity
         cargarDataAVistas();
     }
 
-    private void cargarDataAVistas(){
-        vacioX = 2;
-        vacioY = 2;
+    private void cargarVista(){
+        grupo = findViewById(R.id.grupo);
+        textoViewPasos = findViewById(R.id.textView_Pasos);
+        textoViewTimer = findViewById(R.id.textView_Timer);
+        botonReinicio = findViewById(R.id.botonReinicio);
+
+        botonRegistroGanador = (Button) findViewById(R.id.botonRegistro);
+        botonRegistroGanador.setVisibility(View.INVISIBLE);
+
+        cargoTimer();
+        botones = new Button[3][3];
+
+        for (int i = 0; i<grupo.getChildCount();i++){ //getChildCount retorna la cantidad de, en este caso, botones = 9
+            botones[i/3][i%3] = (Button) grupo.getChildAt(i); //getChildAt retorna la posicion en el grupo para cada boton
+        }
+
+        botonReinicio.setOnClickListener(new View.OnClickListener() { //determino la funcion del boton Reinicio
+            @Override
+            public void onClick(View v) {
+                if (correElTiempo) //si esta corriendo el tiempo, lo detiene e inhabilita seguir moviendo piezas
+                {
+                    timer.cancel();
+                    botonReinicio.setText("Reanudar");
+                    correElTiempo = false;
+                    for (int i = 0; i < grupo.getChildCount(); i++) {
+                        botones[i / 3][i % 3].setClickable(false);
+                    }
+                }else
+                {
+                    cargoTimer(); //si el timer esta detenido, lo reanuda para continuar
+                    botonReinicio.setText("Parar tiempo");
+                    for (int i = 0; i < grupo.getChildCount(); i++) {
+                        botones[i / 3][i % 3].setClickable(true);
+                    }
+
+                }
+            }
+        });
+    }
+
+    private void cargarNumeros(){ //cargo los numeros de cada boton a excepcion del ultimo, al tener 9 botones, lo hago para 8.
+        tiles = new int[9];
         for (int i = 0; i < grupo.getChildCount()-1; i++)
         {
-            botones[i/3][i%3].setText(String.valueOf(tiles[i]));
-            botones[i/3][i%3].setBackgroundResource(android.R.drawable.btn_default);
+            tiles[i] = i+1;
         }
-        botones[vacioX][vacioY].setText("");
-        botones[vacioX][vacioY].setBackgroundColor(ContextCompat.getColor(this, R.color.white));
+
     }
 
     private void generaNumeros(){ //Carga de manera aleatoria con la clase Random el orden inicial de los numeros en el rompecabezas
@@ -69,7 +106,24 @@ public class Juego extends AppCompatActivity
             generaNumeros();
     }
 
+    private void cargarDataAVistas(){
+        vacioX = 2;
+        vacioY = 2;
+        for (int i = 0; i < grupo.getChildCount()-1; i++)
+        {
+            botones[i/3][i%3].setText(String.valueOf(tiles[i]));
+            botones[i/3][i%3].setBackgroundResource(android.R.drawable.btn_default);
+        }
+        botones[vacioX][vacioY].setText("");
+        botones[vacioX][vacioY].setBackgroundColor(ContextCompat.getColor(this, R.color.white));
+    }
+
+
+
     private boolean esSolucionable(){
+        //la teoria indica que "No es posible resolver una instancia de 8 rompecabezas si el número de inversiones es impar en el estado de entrada."
+        //que es una inversion? Un par de fichas forman una inversión si los valores de las fichas están en orden inverso al de su aparición en el estado objetivo.
+        //lo que hacemos en la lógica es verificar el orden y en base a eso definimos si suma una inversion o no entre 2 botones.
         int cuentoInversiones = 0;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < i; j++)
@@ -82,14 +136,7 @@ public class Juego extends AppCompatActivity
         return cuentoInversiones%2 == 0;
     }
 
-    private void cargarNumeros(){
-        tiles = new int[9];
-        for (int i = 0; i < grupo.getChildCount()-1; i++)
-        {
-            tiles[i] = i+1;
-        }
 
-    }
     private void cargoTimer(){ //Cargo el Timer para iniciar el conteo
         correElTiempo = true;
         timer = new Timer();
@@ -113,45 +160,7 @@ public class Juego extends AppCompatActivity
         textoViewTimer.setText(String.format("Tiempo: %02d:%02d:%02d",hor, min, seg));
     }
 
-    private void cargarVista(){
-        grupo = findViewById(R.id.grupo);
-        textoViewPasos = findViewById(R.id.textView_Pasos);
-        textoViewTimer = findViewById(R.id.textView_Timer);
-        botonReinicio = findViewById(R.id.botonReinicio);
 
-        botonRegistroGanador = (Button) findViewById(R.id.botonRegistro);
-        botonRegistroGanador.setVisibility(View.INVISIBLE);
-
-        cargoTimer();
-        botones = new Button[3][3];
-
-        for (int i = 0; i<grupo.getChildCount();i++){
-            botones[i/3][i%3] = (Button) grupo.getChildAt(i);
-        }
-
-        botonReinicio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (correElTiempo)
-                {
-                    timer.cancel();
-                    botonReinicio.setText("Reanudar");
-                    correElTiempo = false;
-                    for (int i = 0; i < grupo.getChildCount(); i++) {
-                        botones[i / 3][i % 3].setClickable(false);
-                    }
-                }else
-                    {
-                        cargoTimer();
-                        botonReinicio.setText("Parar tiempo");
-                        for (int i = 0; i < grupo.getChildCount(); i++) {
-                            botones[i / 3][i % 3].setClickable(true);
-                        }
-
-                    }
-            }
-        });
-    }
 
     public void clickBoton(View view){
         Button button = (Button) view;
